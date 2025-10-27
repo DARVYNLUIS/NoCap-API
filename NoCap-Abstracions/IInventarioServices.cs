473cca0c6f423ad4473c2454181646d7dfd624cf
DARVYNLUIS <darvynlavandier24@gmail.com>
2025-10-27 19:26:43 -0400

    Eliminando entidad inecesaria

--- NoCap-Abstracions/IInventarioServices.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NoCap_Abstracions;
-
-public interface IInventarioServices
-{
-    Task<bool> ActualizarStockAsync(int productoId, int cantidad);
-    Task<bool> RegistrarEntradaAsync(int productoId, int cantidad, int colorId, int tamañoId, int estadoId);
-    Task<int> ObtenerStockDisponibleAsync(int productoId);
-}
--- NoCap-Domain/DTOS/InventarioDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NoCap_Data.Data;
-
-public class InventarioDto
-{
-    public int InventarioId { get; set; }
-    public DateTime FechaAgregado {get;set; }
-    public int CantidadProductos {  get; set; }
-    public List<InventarioDetalleDTO> InventarioDetalles { get; set; } = new List<InventarioDetalleDTO>();
-}
