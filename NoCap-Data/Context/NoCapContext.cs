6803999a4f93103781de4e71da3651d31f7b18d5
DARVYNLUIS <darvynlavandier24@gmail.com>
2025-10-29 21:55:25 -0400

    Eliminando las entidades no necesarias en el contetxto

--- NoCap-Data/Context/NoCapContext.cs
+++ NoCap-Data/Context/NoCapContext.cs
@@ -8,16 +8,12 @@ public class NoCapContext(DbContextOptions<NoCapContext> options) : DbContext(op
     public DbSet<CarritoDetalle> CarritoDetalles { get; set; }
     public DbSet<Carritos> Carritos { get; set; }
     public DbSet<Categorias> Categorias { get; set; }
-    public DbSet<Colores> Colores { get; set; }
     public DbSet<Estados> Estados { get; set; }
-    public DbSet<Inventario> Inventarios { get; set; }
-    public DbSet<InventarioDetalle> InventarioDetalles { get; set; }
     public DbSet<Marcas> Marcas { get; set; }
     public DbSet<OrdenCompra> OrdenCompras { get; set; }
     public DbSet<Pagos> Pagos { get; set; }
     public DbSet<Productos> Productos { get; set; }
     public DbSet<Roles> Roles { get; set; }
-    public DbSet<Tamaños> Tamaños { get; set; }
     public DbSet<Usuarios> Usuarios { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -26,36 +22,13 @@ public class NoCapContext(DbContextOptions<NoCapContext> options) : DbContext(op
           .HasOne(c => c.Estado)
           .WithMany()
           .HasForeignKey(c => c.EstadoId)
-          .OnDelete(DeleteBehavior.NoAction); // 🔥 evita cascadas múltiples
+          .OnDelete(DeleteBehavior.NoAction); 
 
         modelBuilder.Entity<Carritos>()
             .HasOne(c => c.Usuario)
             .WithMany()
             .HasForeignKey(c => c.UsuarioId)
             .OnDelete(DeleteBehavior.Restrict);
-        modelBuilder.Entity<Tamaños>().HasData(
-             new Tamaños
-             {
-                 TamañoId = 1,
-                 nombre = "XL",
-
-             },
-             new Tamaños
-             {
-                 TamañoId = 2,
-                 nombre = "L"
-             },
-             new Tamaños
-             {
-                 TamañoId = 3,
-                 nombre = "M"
-             },
-             new Tamaños
-             {
-                 TamañoId = 4,
-                 nombre = "S"
-             }
-            );
         modelBuilder.Entity<Roles>().HasData(
             new Roles
             {
@@ -116,33 +89,6 @@ public class NoCapContext(DbContextOptions<NoCapContext> options) : DbContext(op
             {
                 EstadoId = 7,
                 Nombre = "Comprado"
-            }
-        );
-        modelBuilder.Entity<Colores>().HasData(
-            new Colores
-            {
-                ColorId = 1,
-                Nombre = "Negro",
-            },
-            new Colores
-            {
-                ColorId = 2,
-                Nombre = "Blanco",
-            },
-            new Colores
-            {
-                ColorId = 3,
-                Nombre = "Gris",
-            },
-            new Colores
-            {
-                ColorId = 4,
-                Nombre = "Azul Marino",
-            },
-            new Colores
-            {
-                ColorId = 5,
-                Nombre = "Beige",
             }
         );
     }
