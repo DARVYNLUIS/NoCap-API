797f9a3a71f2510ca0103dd4a55e8e7556459531
DARVYNLUIS <darvynlavandier24@gmail.com>
2025-11-03 16:30:51 -0400

    corregiendo sintaxis

--- NoCap-Domain/DTOS/ProductosDto.cs
+++ NoCap-Domain/DTOS/ProductosDto.cs
@@ -7,7 +7,7 @@ public class ProductosDto
     public string ProductoDescripcion { get; set; } = string.Empty;
     public string ProductoImagne { get; set; } = string.Empty;
     public DateTime FechaCreacionProducto { get; set; } = DateTime.Now;
-    public bool Activo { get; set; };
+    public bool Activo { get; set; }
     public int Stocks { get; set; }
     public double PrecioProductoVenta { get; set; }
     public int CategoriaId { get; set; }
