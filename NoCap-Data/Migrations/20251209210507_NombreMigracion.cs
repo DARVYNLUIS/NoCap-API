9ccbab9a9a56de37b90201f793edbe8c2e9bf5c6
DARVYNLUIS <darvynlavandier24@gmail.com>
2025-12-09 17:09:38 -0400

    Fix nueva migracion para los cambios en los modelos

---
+++ NoCap-Data/Migrations/20251209210507_NombreMigracion.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoCap_Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NombreMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FechaCreacionProducto",
+                table: "Productos",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FechaDeCompra",
+                table: "OrdenCompras",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FechaCreacion",
+                table: "Carritos",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FechaCreacionProducto",
+                table: "Productos",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FechaDeCompra",
+                table: "OrdenCompras",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FechaCreacion",
+                table: "Carritos",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
